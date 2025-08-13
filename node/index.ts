e2a4b2abdb11d405d4653dc8e9ed16cf465f82c5
Gabriel Gotardo <87941903+gabgotardo@users.noreply.github.com>
2025-08-13 16:56:45 -0300

    Integrate clients into PaymentProviderService configuration
    - Added clients import to the PaymentProviderService instantiation.
    - Ensured the service utilizes the newly defined clients for improved functionality.

--- node/index.ts
+++ node/index.ts
@@ -1,7 +1,9 @@
 import { PaymentProviderService } from '@vtex/payment-provider'
 
+import { clients } from './clients'
 import TestSuiteApprover from './connector'
 
 export default new PaymentProviderService({
+  clients,
   connector: TestSuiteApprover,
 })
